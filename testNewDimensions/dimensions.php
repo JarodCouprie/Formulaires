78fc74f322512c6bb6dd8132a7835890154bc4f1
Spidw3b <jarod64@hotmail.fr>
2023-03-28 14:40:09 +0200

    resize working

--- testNewDimensions/dimensions.php
+++ testNewDimensions/dimensions.php
@@ -1,7 +1,6 @@
 <?php
 
 function load_image($filename, $width, $height, $newfilename) {
-   $image_p = imagecreatetruecolor($width, $height);
    // Cacul des nouvelles dimensions
    list($width_orig, $height_orig, $type) = getimagesize($filename);
 
@@ -12,30 +11,30 @@ function load_image($filename, $width, $height, $newfilename) {
    } else {
       $height = $width/$ratio_orig;
    }
-   echo $width, $height;
+   $image_p = imagecreatetruecolor($width, $height);
+   echo $width."x".$height;
    // Détermine le type de l'image et la crée
    if( $type == IMAGETYPE_JPEG ) {
        $image = imagecreatefromjpeg($filename);
-       echo $width, $height;
        imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
        imagejpeg($image_p, $newfilename);
-       echo "jpeg";
+       echo ".jpeg";
    };
    if( $type == IMAGETYPE_PNG ) {
        $image = imagecreatefrompng($filename);
        imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
-       imagepng($image,$newfilename);
-       echo "png";
+       imagepng($image_p,$newfilename);
+       echo ".png";
    };
    if( $type == IMAGETYPE_GIF ) {
        $image = imagecreatefromgif($filename);
        imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
-       imagegif($image, $newfilename);
-       echo "gif";
+       imagegif($image_p, $newfilename);
+       echo ".gif";
    };
 };
 
-load_image("./test.jpg", 1600, 1200, "XLTest.jpg");
+load_image("./testGIF.gif", 400, 400, "SMtestGIF.gif");
 
 
 
