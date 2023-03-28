23a1661379df54aee0e9f0495634d0805a44a0c9
Spidw3b <jarod64@hotmail.fr>
2023-03-28 16:43:46 +0200

    crop or not

--- testNewDimensions/dimensions.php
+++ testNewDimensions/dimensions.php
@@ -1,6 +1,9 @@
 <?php
 
-function load_image($filename, $width, $height, $newfilename) {
+function loadImage($filename, $width, $height, $newfilename, $croped) {
+    // SM => 400 x 300 px
+    // MD => 800x 600 px
+    // XL => 1200 x 800 px
    // Cacul des nouvelles dimensions
    list($width_orig, $height_orig, $type) = getimagesize($filename);
 
@@ -12,6 +15,8 @@ function load_image($filename, $width, $height, $newfilename) {
       $height = $width/$ratio_orig;
    }
    $image_p = imagecreatetruecolor($width, $height);
+   echo $width_orig."x".$height_orig;
+   echo "</br>";
    echo $width."x".$height;
    // Détermine le type de l'image et la crée
    if( $type == IMAGETYPE_JPEG ) {
@@ -33,11 +38,55 @@ function load_image($filename, $width, $height, $newfilename) {
        echo ".gif";
    };
 };
+function cropImage($filename,$newfilename){
+    // Cacul des nouvelles dimensions
+    list($width_orig, $height_orig, $type) = getimagesize($filename);
 
-load_image("./testGIF.gif", 400, 400, "SMtestGIF.gif");
+    $abscisses = 0;
+    $ordonnees = 0;
+    if ($width_orig > $height_orig){
+        $width = $height_orig;
+        $height = $height_orig;
+        $abscisses = ($width_orig - $height_orig)/2;
+    }else{
+        $height = $width_orig;
+        $width = $width_orig;
+        $ordonnees = ($height_orig - $width_orig)/2;
+    };
+    echo "x = ".$abscisses, "y = ".$ordonnees;
+    echo "x = ".$width, "y = ".$height;
+    $image_p = imagecreatetruecolor($width, $height);
+    if( $type == IMAGETYPE_JPEG ) {
+        $image = imagecreatefromjpeg($filename);
+        imagecopyresampled($image_p, $image, 0, 0, $abscisses, $ordonnees, $width, $height, $width, $height);
+        imagejpeg($image_p, $newfilename);
+    };
+    if( $type == IMAGETYPE_PNG ) {
+        $image = imagecreatefrompng($filename);
+        imagecopyresampled($image_p, $image, 0, 0, $abscisses, $ordonnees, $width, $height, $width, $height);
+        imagepng($image_p,$newfilename);
+    };
+    if( $type == IMAGETYPE_GIF ) {
+        $image = imagecreatefromgif($filename);
+        imagecopyresampled($image_p, $image, 0, 0, $abscisses, $ordonnees, $width, $height, $width, $height);
+        imagegif($image_p, $newfilename);
+    };
+};
+
+function cropImageS($filename,$newfilename){
+    $im = imagecreatefromjpeg($filename);
+    $size = min(imagesx($im), imagesy($im));
+    $im2 = imagecrop($im, ['x' => 0, 'y' => 0, 'width' => $size, 'height' => $size]);
+    if ($im2 !== FALSE) {
+        imagejpeg($im2, $newfilename);
+    }
+}
 
 
+//loadImage("./testGIF.gif", 400, 400, "SMtestGIF.gif");
 
+cropImage("./testJPG.jpg", "CropedTEST.jpg");
 
+//cropImageS("./testJPG.jpg","TEEEEEEEEST.jpg");
 
 ?>
